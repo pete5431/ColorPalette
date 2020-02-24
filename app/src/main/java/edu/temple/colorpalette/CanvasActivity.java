90141e0a39cf947a2463e75c1784e5c17060ac7e
pete5431 <scholarpetez@gmail.com>
2020-02-24 11:23:23 -0500

    Created Activity file for Canvas, and added an xml file for its layout. Implemented onCreate in CanvasActivity.

--- app/src/main/java/edu/temple/colorpalette/CanvasActivity.java
+++ app/src/main/java/edu/temple/colorpalette/CanvasActivity.java
@@ -1,8 +1,19 @@
 package edu.temple.colorpalette;
 
-public class CanvasActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+
+        
+
+    }
 
 
 }
