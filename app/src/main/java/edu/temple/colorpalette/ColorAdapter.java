40e49839130b29b5282d01f73c9c97dcdde93345
pete5431 <scholarpetez@gmail.com>
2020-02-26 13:35:38 -0500

    Added two new methods, getNormalView and getInitialView. getInitialView returns the initial view to be displayed on the spinner. getNormalView returns the view that would normally be seen. Increased the getCount size by 1 because of the initial view. Added position decrement in getNormalView to account for proper access to the string array.

--- app/src/main/java/edu/temple/colorpalette/ColorAdapter.java
+++ app/src/main/java/edu/temple/colorpalette/ColorAdapter.java
@@ -13,21 +13,22 @@ public class ColorAdapter extends BaseAdapter {
     private Context context;
     private String[] colors;
     // The selected item position.
-    private int itemSelectedIndex = 0;
+    private int itemSelectedPosition = 0;
 
     public ColorAdapter(Context c, String[] colors){
         this.colors = colors;
         this.context = c;
     }
 
-    // Sets the selected item position to the passed index value.
-    public void setItemSelectedIndex(int index){
-        itemSelectedIndex = index;
+    // Sets the selected item position to the passed position value.
+    public void setItemSelectedPosition(int position){
+        itemSelectedPosition = position;
     }
 
     @Override
     public int getCount() {
-        return colors.length;
+        // The size will be +1 because an extra initial view is included.
+        return colors.length + 1;
     }
 
     @Override
@@ -43,11 +44,56 @@ public class ColorAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        // If position is 0, return an initial view.
+        if(position == 0){
+            return getInitialView(false);
+        }else
+        // Returns the view that will act as the item for the spinner.
+        return getNormalView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+
+        // If position is 0, return an initial view with if_drop set to true.
+        if(position == 0){
+            return getInitialView(true);
+        }else {
+            // Generate a normal view.
+            View view = getNormalView(position, convertView, parent);
+            // If the selected position is equal to the position, set the text view background to white.
+            if (itemSelectedPosition == position) {
+                view.findViewById(R.id.text_color).setBackgroundColor(Color.WHITE);
+            }
+            return view;
+        }
+    }
+
+    // Returns an initial view that acts as the first item of the spinner.
+    public View getInitialView(boolean if_drop){
+        // The initial view will be a simple text view.
+        TextView initial = new TextView(context);
+
+        // If the view is being dropped in dropdown view, make the height 0 so the user can't see it.
+        if(if_drop) {
+            initial.setHeight(0);
+        }
+        else{
+            // Set the appearance of the initial prompt.
+            initial.setText(R.string.spinner_prompt);
+            initial.setTextSize(20);
+            initial.setBackgroundColor(Color.LTGRAY);
+        }
+        return initial;
+    }
+
+    // Returns normal view that is not an initial view.
+    public View getNormalView(int position, View convertView, ViewGroup parent){
+
         View view;
 
-        // Makes sure the views are recycled instead generating new ones each time.
-        // Makes sure the convertView is not null before recycling.
-        if(convertView == null){
+        // Makes sure the convertView is not null or not an initial text view before recycling.
+        if(convertView == null || convertView instanceof TextView){
             LayoutInflater inflater = LayoutInflater.from(context);
             view = inflater.inflate(R.layout.spinner_color_item, parent, false);
         }
@@ -56,28 +102,14 @@ public class ColorAdapter extends BaseAdapter {
         // Get the text view from the spinner item layout.
         TextView colorText = view.findViewById(R.id.text_color);
 
+        // Since the initial position 0 is ignored and a special view is returned, we subtract one from subsequent positions.
+        position = position - 1;
+
         // Set the text of the text view to the color name.
         colorText.setText(colors[position]);
 
-        // Set the top view to be always white because it is selected.
+        // Set the background color of the text view.
         colorText.setBackgroundColor(Color.parseColor(colors[position]));
-
-        // Returns the view that will act as the item for the spinner.
-        return view;
-    }
-
-    @Override
-    public View getDropDownView(int position, View convertView, ViewGroup parent) {
-
-        // Get the view from getView, this is to avoid repetitive code.
-        View view = getView(position, convertView, parent);
-
-        TextView colorText = view.findViewById(R.id.text_color);
-
-        // If the position is the selected item, set text view background to white, else set to the color.
-        if(position == itemSelectedIndex){
-            colorText.setBackgroundColor(Color.WHITE);
-        }else colorText.setBackgroundColor(Color.parseColor(colors[position]));
 
         return view;
     }
