a15a3fdbf2005ba116a53be2fff8f52e74aae8f0
pete5431 <scholarpetez@gmail.com>
2020-03-20 15:00:35 -0400

    Added spinner code to PaletteFragment onCreateView(). Implemented interface to communicate with PaletteActivity.

--- app/src/main/java/edu/temple/colorpalette/PaletteFragment.java
+++ app/src/main/java/edu/temple/colorpalette/PaletteFragment.java
@@ -1,7 +1,7 @@
 package edu.temple.colorpalette;
 
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,26 +9,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 
 public class PaletteFragment extends Fragment {
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // the fragment initialization parameters for color id array and color name array.
     private static final String COLOR_ID_ARRAY_TAG = "colorIdArray";
     private static final String COLOR_NAME_ARRAY_TAG = "colorNameArray";
-
 
     private String[] colorIdArray;
     private String[] colorNameArray;
 
-    private OnFragmentInteractionListener mListener;
+    ColorAdapter colorAdapter;
+    int itemSelectedPosition = 0;
+
+    CanvasFragment canvas;
+
+    private OnItemSelectedInterface parentActivity;
 
     public PaletteFragment() {
         // Required empty public constructor
     }
-
-
 
     public static PaletteFragment newInstance(String[] colorIdArray, String[] colorNameArray) {
         PaletteFragment fragment = new PaletteFragment();
@@ -51,46 +54,70 @@ public class PaletteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View fragmentView = inflater.inflate(R.layout.fragment_palette, container, false);
+
+        Spinner colorSpinner = fragmentView.findViewById(R.id.spinner_color);
+
+        colorAdapter = new ColorAdapter(this.getActivity(), colorIdArray, colorNameArray);
+
+        colorSpinner.setAdapter(colorAdapter);
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    return;
+                }
+                else{
+                    itemSelectedPosition = position;
+                    position = position - 1;
+                }
+
+                view.findViewById(R.id.text_color).setBackgroundColor(Color.WHITE);
+
+                // Set the item selected index variable of color adapter to the index.
+                colorAdapter.setItemSelectedPosition(itemSelectedPosition);
+
+                // Calls getItem of ColorAdapter for the data contained at the position of the string array.
+                String chosenColorId = parent.getItemAtPosition(position).toString();
+
+                // Get the chosen color name based off the position.
+                String chosenColorName = colorAdapter.getColorName(position);
+
+                parentActivity.itemClicked(chosenColorId, chosenColorName);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_palette, container, false);
+        return fragmentView;
     }
 
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnItemSelectedInterface) {
+            parentActivity = (OnItemSelectedInterface) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnItemSelectedInterface");
         }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        parentActivity = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public interface OnItemSelectedInterface {
+        void itemClicked(String chosenColorId, String chosenColorName);
     }
 }
